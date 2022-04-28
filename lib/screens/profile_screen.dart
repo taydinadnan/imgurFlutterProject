310f8e35991a392283a0224f6d26cfc8c016456e
Turgay Aydin <taydinadnan@gmail.com>
2022-04-28 15:01:27 +0200

    .

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -10,6 +10,10 @@ class ProfileScreen extends StatefulWidget {
 class _ProfileScreenState extends State<ProfileScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: Text('Profile'),
+      ),
+    );
   }
 }
